345f107df8349095709043e6216c5767aded7ad3
Amirbek2506 <alikmirzoev1999@gmail.com>
2021-09-20 17:53:53 +0500

    chacks status payment to Avg and TotalInCategory

--- pkg/stats/example_test.go
+++ pkg/stats/example_test.go
@@ -2,7 +2,7 @@ package stats
 
 import (
 	"fmt"
-	"github.com/Amirbek2506/bank/pkg/types"
+	"github.com/Amirbek2506/bank/v2/pkg/types"
 )
 
 func ExampleAvg()  {
@@ -11,18 +11,20 @@ func ExampleAvg()  {
 			ID:       1,
 			Amount:   100,
 			Category: "pay",
+			Status: types.StatusOk,
 		},
 		{
 			ID:       2,
 			Amount:   200,
 			Category: "pay",
+			Status: types.StatusFail,
 		},
 	}
 
 		fmt.Println(Avg(payments))
 
 	// Output:
-	// 150
+	// 100
 }
 
 
@@ -31,24 +33,27 @@ func ExampleTotalInCategory()  {
 	fmt.Println(TotalInCategory([]types.Payment{
 		{
 			ID:       1,
-			Amount:   3,
+			Amount:   300,
 			Category: "pay",
+			Status: types.StatusOk,
 		},
 		{
 			ID:       2,
 			Amount:   200,
 			Category: "fee",
+			Status: types.StatusInProgress,
 		},
 		{
 			ID:       3,
-			Amount:   4,
+			Amount:   444444,
 			Category: "pay",
+			Status: types.StatusFail,
 		},
 	} ,
-		types.Category("fee")))
+		types.Category("pay")))
 
 	// Output:
-	// 200
+	// 300
 }
 
 
@@ -59,16 +64,19 @@ func ExampleTotalInCategory_empty()  {
 				ID:       1,
 				Amount:   300,
 				Category: "pay",
+				Status: types.StatusFail,
 			},
 			{
-			ID:       2,
+				ID:       2,
 				Amount:   200,
 				Category: "fee",
+				Status: types.StatusOk,
 			},
 			{
 			ID:       3,
 				Amount:   200,
 				Category: "pay",
+				Status: types.StatusInProgress,
 			},
 	}
 
--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -1,19 +1,26 @@
 package stats
 
-import "github.com/Amirbek2506/bank/pkg/types"
+import "github.com/Amirbek2506/bank/v2/pkg/types"
 
 func Avg(payments []types.Payment) types.Money {
 	var summa types.Money = 0
+	var count types.Money = 0
 	for _, payment := range payments {
-		summa+=payment.Amount
+		if payment.Status != types.StatusFail{
+			summa+=payment.Amount
+			count++
+		}
 	}
-	return summa/types.Money(len(payments))
+	if count==0{
+		return 0
+	}
+	return summa/count
 }
 
 func TotalInCategory(payments []types.Payment, category types.Category) types.Money {
 	var summa types.Money = 0
 	for _, payment := range payments {
-		if payment.Category == category{
+		if payment.Category == category && payment.Status != types.StatusFail{
 			summa+=payment.Amount
 		}
 	}
